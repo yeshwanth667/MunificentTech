af2728de327cf127f43c63110d80905648c454cb
yeshwanth667 <144303906+yeshwanth667@users.noreply.github.com>
2025-08-03 00:08:52 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 // src/services/api.js
 import axios from 'axios';
 
-const API_BASE = 'http://localhost:5000/api';
+const API_BASE = 'https://munificenttech-api.onrender.com/api';
 
 export const getCourses = () => axios.get(`${API_BASE}/courses`);
 export const fetchEnrollments = () => axios.get(`${API_BASE}/enrollments/me`);
